12d3eda885e11b3f695a5ade4acb2f3589609829
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-19 00:23:23 +0200

    catalog and data get fetch added

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
 import BootsCard from "../bootsCard/BootsCard.jsx";
-import request from "../../utils/request.js";
-
+import useReq from "../../hooks/useReq.js";
 export default function Catalog() {
 
     const [articles, setArticles] = useState([]);
+    const { request } = useReq();
     
     useEffect(() => {
         (async () => {
             try {
-                const result = request('/boots', 'get')
+                const result = await request('/data/boots', 'get')
+                console.log('BOOTS    ',result)
                 setArticles(result)
             } catch (error) {
                 alert(error.message)
             }
-        })
-    })
+        })();
+    },[])
     return (
         <section className="catalog-page">
             <h1>Catalog</h1>
+            {articles.length === 0 && <h3>No boots uploaded</h3>}
             <div className="catalog-container">
                 {articles.map(article => <BootsCard key={article._id} {...article}/>)}
             </div>
