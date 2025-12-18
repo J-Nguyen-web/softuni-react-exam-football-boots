242a63ae69bb4e6f57c9db4e06b45882a552efce
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-19 00:21:49 +0200

    guest and logged user nav added
    depends from login status, navigation differs

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,26 @@
+import { Link } from "react-router-dom";
+import { useUserContext } from "../../context/UserContext.jsx";
+
 export default function Header() {
+    const { isAuth } = useUserContext();
     return (
         <header className="nav Link home">
             <nav>
-                <Link className="home" to="/"><img src="" alt="" /></Link>
-                <Link to="/catalog">Catalog</Link>
-                <div id="user">
-                    <Link to="boots/create">Create boots</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                <div className="guests">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                <Link className="home" to="/"><img alt="" /></Link>
+                <Link to="/catalog" className="nav-link">Catalog</Link>
+                {isAuth
+                    ? (
+                        <div id="user">
+                            <Link to="/create" className="nav-link">Create boots</Link>
+                            <Link to="/logout" className="nav-link">Logout</Link>
+                        </div>
+                    )
+                    : (
+                        <div className="guests">
+                            <Link to="/login" className="nav-link">Login</Link>
+                            <Link to="/register" className="nav-link">Register</Link>
+                        </div>
+                    )}
             </nav>
         </header>
     );
