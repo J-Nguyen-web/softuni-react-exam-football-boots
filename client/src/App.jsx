56585dba188fa80537d377b2ef8572152bc3d565
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-19 00:23:55 +0200

    adding paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,30 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home.jsx'
 import Login from './components/login/Login.jsx'
+import Header from './components/header/Header.jsx'
+import Footer from './components/footer/Footer.jsx'
+import Register from './components/register/Register.jsx'
+import Logout from './components/logout/Logout.jsx'
+import BootsCreate from './components/boots-create/BootsCreate.jsx'
+import Catalog from './components/catalog/Catalog.jsx'
 
 function App() {
 
   return (
     <>
+
+    <Header />
+
       <Routes >
         <Route path='/' element={< Home />} />
+        <Route path='/create' element={< BootsCreate />} />
+        <Route path='/catalog' element={< Catalog />} />
+        <Route path='/register' element={< Register />} />
         <Route path='/login' element={< Login />} />
+        <Route path='/logout' element={< Logout />} />
       </Routes>
+    
+    <Footer />
     </>
   )
 }
