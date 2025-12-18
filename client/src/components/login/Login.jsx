419b8232c7a9a7b2d41815e50694bef7e95e6876
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-19 00:22:49 +0200

    login added

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,10 +1,47 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../context/UserContext.jsx";
+import useForm from "../../hooks/useForm.js";
+
 export default function Login() {
+
+  const navigate = useNavigate();
+  const { loginHandler } = useContext(UserContext)
+
+  const loginFormHandler = async ({ email, password }) => {
+    if (!email || !password) {
+      return alert('Email and password are required!')
+      // TODO MODAL
+    }
+
+    try {
+      await loginHandler(email, password)
+      navigate('/')
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  const {
+    inputData,
+    formAction
+  } = useForm(loginFormHandler, {
+    email: '',
+    password: '',
+  });
+
   return (
-    <div className="page form-page">
-      <h2>Login</h2>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
-    </div>
+    <section id="login-page">
+      <form action={formAction} id="login">
+        <div className="container">
+          <h1>Login</h1>
+          <label htmlFor="login-email">Email</label>
+          <input type="text" id="login-email" {...inputData('email')} />
+          <label htmlFor="login-password">Password</label>
+          <input type="text" id="login-password" {...inputData('password')} />
+          <input type="submit" className="btn submit" value="Login" />
+        </div>
+      </form>
+    </section>
   );
 }
