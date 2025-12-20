996d9a8589c5854a1b89802f830b4a3ef978d1b1
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-20 19:18:12 +0200

    add validation for user entries login register

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,38 +1,58 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, Links, useNavigate } from "react-router-dom";
 import UserContext from "../../context/UserContext.jsx";
-import useForm from "../../hooks/useForm.js";
+// import useForm from "../../hooks/useForm.js";
+import useForm from "../../hooks/useFormValidate.js";
 import { useModal } from "../../context/ModalContext.jsx";
+import { parseServerError } from "../../util/errorHandler.js";
+import { validateUser } from "../../validators/validateUser.js";
 
 export default function Login() {
 
   const navigate = useNavigate();
-  const {showModal} = useModal()
+  const { showModal } = useModal()
   const { loginHandler } = useContext(UserContext)
 
   const loginFormHandler = async ({ email, password }) => {
-    if (!email || !password) {
+    if (email =='' || password == '') {
       return showModal('Email and password are required!', "error")
       // TODO MODAL
     }
 
     try {
       await loginHandler(email, password);
-      showModal('Welcome', "succes")
+      showModal(`Welcome ${email}`, "succes")
 
       navigate('/')
-    } catch (error) {
-      showModal(error.message, "error")
+    } catch (error) { // todo forbiden msg
+
+      showModal(
+        <span>Wrong email or password! <br />
+          <br />
+          If not registered,<br />
+          please <Link to="/register">follow the link</Link>
+        </span>, "error"
+        )
     }
   }
 
   const {
     inputData,
-    formAction
+    formAction,
+    errors,
+    touched,
   } = useForm(loginFormHandler, {
     email: '',
     password: '',
-  });
+  }, validateUser);
+
+  const inputClass = (field) =>
+    `form-input ${errors[field] && touched[field] ? "input-error" : ""}`
+
+  const errorText = (field) =>
+    errors[field] && touched[field] && (
+      <p className="error-text">{errors[field]}</p>
+    );
 
   return (
     <section className="login-page">
@@ -40,10 +60,13 @@ export default function Login() {
         <div className="login-form">
           <h1>Login</h1>
           <label htmlFor="login-email">Email</label>
-          <input type="email" id="login-email" {...inputData('email')} />
+          <input type="email" id="login-email" {...inputData('email')} className={inputClass("email")} />
+          {errorText("email")}
           <label htmlFor="login-password">Password</label>
-          <input type="password" id="login-password" {...inputData('password')} />
+          <input type="password" id="login-password" {...inputData('password')} className={inputClass("password")} />
+          {errorText("password")}
           <input type="submit" className="btn-login" value="Login" />
+          <p>Not part of a team? Come,  <Link to={`/register`} ><span className="linkTo">join us</span></Link>.</p>
         </div>
       </form>
     </section>
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,39 +1,57 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import UserContext from "../../context/UserContext.jsx";
-import useForm from "../../hooks/useForm.js";
+// import useForm from "../../hooks/useForm.js";
+import useForm from "../../hooks/useFormValidate.js";
+import { validateUser } from "../../validators/validateUser.js";
+import { useModal } from "../../context/ModalContext.jsx";
 
 export default function Register() {
     const navigate = useNavigate();
+    const {showModal} = useModal();
     const { registerHandler } = useContext(UserContext)
 
     const registerFormHandler = async (values) => {
         const { email, password, rePassword } = values;
-
+console.log('email')
         if (!email || !password ) {
-            return alert('Email and password are required!')
+            
+             showModal('Email and password are required!', "error");
+             return;
         }
 
         if (password !== rePassword ) {
-            return alert('Password missmatch!')
+            console.log('password')
+             showModal('Password missmatch!', "error")
+             return;
         }
 
         try {
             const user = await registerHandler(email, password);
             navigate('/')
         } catch (err) {
-            alert(err.message);
+            showModal(err.message);
         }
     }
 
     const {
         inputData,
         formAction,
+        errors,
+        touched,
     } = useForm(registerFormHandler, {
         email: '',
         password: '',
         rePassword: '',
-    });
+    }, validateUser);
+
+  const inputClass = (field) =>
+    `form-input ${errors[field] && touched[field] ? "input-error" : ""}`
+
+  const errorText = (field) =>
+    errors[field] && touched[field] && (
+      <p className="error-text">{errors[field]}</p>
+    );    
     
     return (
         <section className="register-page">
@@ -41,15 +59,18 @@ export default function Register() {
                 <div className="register-form">
                     <h1>Register</h1>
                     <label htmlFor="register-email">Email:</label>
-                    <input type="email" id="register-email" {...inputData('email')}/>
+                    <input type="email" id="register-email" {...inputData('email')} className={inputClass("email")} />
+                    {errorText("email")}
 
                     <label htmlFor="register-password">Password</label>
-                    <input type="password" id="register-password" {...inputData('password')}/>
-
+                    <input type="password" id="register-password" {...inputData('password')} className={inputClass("password")} />
+                    {errorText("password")}
                     <label htmlFor="re-password">Confirm Password:</label>
                     <input type="password" id="re-password" {...inputData('rePassword')}/>
                     
                     <input type="submit" className="btn-register" value="Register"/>
+                    <p>Already in the club? Put your boots and <Link to={`/login`} ><span className="linkTo">go to play</span></Link>.</p>
+
                 </div>
             </form>
         </section>
