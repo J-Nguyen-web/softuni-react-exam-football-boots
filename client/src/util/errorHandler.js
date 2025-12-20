9da0dc1f0eae06c87115dd55de6a1badc94db256
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-20 19:17:06 +0200

    helper for user friendly server errors

--- client/src/util/errorHandler.js
+++ client/src/util/errorHandler.js
@@ -5,8 +5,25 @@ export function parseServerError(error) {
         switch (status) {
             case 400:
                 return data?.message || "Invalid request, PLease check your input.";
-            case :
-                return
+            
+            case 401:
+                return "You must be logged in to perform this actions.";
+            case 403:
+                return "You dont got permission to perform this action.";
+            case 404:
+                return "Requested resource not found.";
+            case 409:
+                return data?.message || "Conflict occured. Try again.";
+            case 500:
+                return "Something went wrong on our side.PLease try again later.";
+            default :
+                return "An unexpected error occured. Please try again.";
         }
     }
+
+    if (error.request) {
+        return "Network error. Please check your internet connection.";
+    }
+
+    return error.message || "An unexpected error occurred.";
 }
