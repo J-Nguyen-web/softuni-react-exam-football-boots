93d5a2693d8f9b2440f25bb570ab4a8ffa6e55b4
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-26 04:08:08 +0200

    most liked boots logic added

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,17 +1,73 @@
+import { useEffect, useState } from "react";
+import { useModal } from "../../context/ModalContext.jsx";
+import { parseServerError } from "../../util/errorHandler.js";
+import Roulette from "../Roulette.jsx";
+import BootsCard from "../bootsCard/BootsCard.jsx";
+import useReq from "../../hooks/useReq.js";
+
 export default function Home() {
+
+    const { showModal } = useModal();
+    const { request } = useReq();
+    const [boots, setBoots] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        let isMounted = true;
+
+        async function mostLikedBoots(limit = 3) {
+            try {
+                const [boots, likes] = await Promise.all([
+                    request(`/data/boots`),
+                    request(`/data/likes`),
+                ]);
+
+                const likesCount = likes.reduce((boots, likes) => {
+                    boots[likes.bootsId] = (boots[likes.bootsId] || 0) + 1;
+                    console.log(boots)
+                    return boots
+                }, {});
+
+                const sortedBoots = boots
+                    .map(boots => ({ ...boots, likes: likesCount[boots._id] || 0 }))
+                    .sort((a, b) => b.likes - a.likes)
+                    .slice(0, limit);
+
+
+                if (isMounted) setBoots(sortedBoots);
+
+            } catch (err) {
+                console.log('on home')
+                showModal(parseServerError(err), "error");
+            } finally {
+                if (isMounted) setLoading(false);
+            }
+        }
+
+        mostLikedBoots();
+
+        return () => {
+            isMounted = false;
+        }
+    }, []);
+
+    if (loading) {
+        return <Roulette />
+    }
+
     return (
         <div className="home-page">
             <div className="home-text">
                 <h1>Welcome to </h1>
                 <p>Discover the latest football boots with ratings, comments, and more.</p>
-                {/* <section className="catalog-page">
-                    <h1>Catalog</h1>
-                    {articles.length === 0 && <h3>No boots uploaded</h3>}
-                    <div className="catalog-container">
-                        {articles.map(article => <BootsCard key={article._id} {...article} />)}
-                    </div>
-                </section> */}
             </div>
+            <section className="home-top3boots">
+                <h1>Top 3</h1>
+                {boots.length === 0 && <h3>No boots uploaded</h3>}
+                <div className="top3boots">
+                    {boots.map(bootsResult => (<BootsCard key={bootsResult._id} {...bootsResult} />))}
+                </div>
+            </section>
         </div>
     );
 }
