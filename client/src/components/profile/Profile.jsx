ffb4ab8779439cb83bb7e261020ddb70bd13650b
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-01-05 23:47:34 +0200

    fetch boots likes comments by user
    loading profile page with personal information

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,9 +1,62 @@
-import UserContext from "../../context/UserContext.jsx";
+import { useEffect, useState } from "react";
+import { useUserContext } from "../../context/UserContext.jsx";
+import useReq from "../../hooks/useReq.js";
 import BootsCard from "../bootsCard/BootsCard.jsx";
+import { useModal } from "../../context/ModalContext.jsx";
+import Roulette from "../Roulette.jsx";
 
 export default function Profile() {
 
-    const {user} = UserContext
+    const {user} = useUserContext();
+    const {request} = useReq();
+    const {showModal} = useModal();
+
+    const [createdBoots, setCreatedBoots] = useState();
+    const [likedBoots, setLikedBoots] = useState();
+    const [comments, setCommnets] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() =>{
+        let mounted = true;
+
+        (async () => {
+            try {
+                const [ boots, likes, comments ] = await Promise.all([
+                    request(`/data/boots?where=${encodeURIComponent(`_ownerId="${user._id}"`)}`),
+                    request(`/data/likes?where=${encodeURIComponent(`userId="${user._id}"`)}`),
+                    request(`/data/comments?where=${encodeURIComponent(`_ownerId="${user._id}"`)}`),
+                ]);
+                console.log('after')
+
+                if(!mounted) return;
+                setCreatedBoots(boots);
+                setCommnets(comments);
+
+                const likedBootsIds = likes.map( like => like.bootsId);
+                const liked = await Promise.all(
+                    likedBootsIds.map(id => request(`/data/boots/${id}`))
+                );
+
+                setLikedBoots(liked);
+                setLoading(false);
+                
+                console.log(boots)
+                console.log(likes)
+                console.log(comments)
+                
+            } catch (error) {
+                alert('error')
+                showModal(error)
+                setLoading(false);
+            }
+        })(); 
+
+        return ()=> (mounted = false)
+    }, []);
+    
+    if(loading){
+        return <Roulette />
+    }
 
     return (
         <section className="profile-page">
@@ -17,7 +70,7 @@ export default function Profile() {
                 <h3>"Created Boots"</h3>
                 <div className="profile-grid">
                     {createdBoots.map(boots => (
-                        <BootsCard key={boots._id} {...boots} />
+                        <BootsCard key={boots?._id} {...boots} />
                     ))}
                 </div>
             </section>
@@ -27,7 +80,7 @@ export default function Profile() {
                 {likedBoots.lenght === 0 && <p>No liked boots yet.</p>}
                 <div className="profile-grid">
                     {likedBoots.map( boots => (
-                        <BootsCard key={boots._id} {...boots} />
+                        <BootsCard key={boots?._id} {...boots} />
                     ))}
                 </div>
             </section>
@@ -36,8 +89,8 @@ export default function Profile() {
                 <h3>Comments</h3>
                 {comments.lenght === 0 && <p>No comments yet.</p>}
                 <ul className="profile-comments">
-                    <li key={comment._id}>
-                        <strong>{comment.bootTitle}: {comment.text}</strong>
+                    <li key={comments?._id}>
+                        <strong>{comments.bootTitle}: {comments.text}</strong>
                     </li>
                 </ul>
             </section>
